acc5811d3d4f099e969f03a42ba473e38003af26
Blake Matthew <79722560+BlakeMatthew32@users.noreply.github.com>
2024-01-07 23:44:16 +0000

    feature: fetch the deck from deck of cards api

--- src/components/PlayArea.jsx
+++ src/components/PlayArea.jsx
@@ -2,10 +2,16 @@
 import Betting from "../pages/betting"
 import Bank from "./bank"
 
+// hooks
+import useDeck from "../hooks/useDeck"
+
 const PlayArea = () => {
+
+    const {value: deck} = useDeck()
+
     return (
         <main className="main">
-            <p className="main__cards-remaining">Cards Remaining: 0</p>
+            <p className="main__cards-remaining">Cards Remaining: {deck ? deck.remaining : 0}</p>
             <Betting />
             <Bank />
         </main>
--- src/hooks/useDeck.jsx
+++ src/hooks/useDeck.jsx
@@ -1,18 +1,10 @@
-import { useRef } from "react";
+
 import useFetch from "./useFetch";
 
 let DECK_API_URL = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2"
 
 export default function useDeck(deckId = "", options = {}, dependencies = []) {
 
-    const firstsRenderRef = useRef(true)
-    const fetchRef = {...useFetch(DECK_API_URL, options, dependencies)}
+    return useFetch(DECK_API_URL, options, dependencies)
 
-    if(!firstsRenderRef.current) {
-        DECK_API_URL = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-    } else {
-        firstsRenderRef.current = false
-    }
-
-    return fetchRef
 }
